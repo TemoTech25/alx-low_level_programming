f3f9f7f73ba37e08504844193936b9e61c14a132
Temo <temotech25@gmail.com>
2023-10-14 08:12:15 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,20 @@ int main(void)
 	for (tens = '0'; tens <= '9'; tens++)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
+		{
 			
 			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
 
-				if (!(ones == '9' && tens == "8'));
+				if (!(ones == '9' && tens == '8'));
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
+		}
 	}
 
 	putchar('\n');
