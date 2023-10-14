c3594de11e6e3e02b5fbabfbd38f113fd7b14153
Temo <temotech25@gmail.com>
2023-10-14 15:56:35 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,26 +10,28 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
+	int hundreds '0';
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (hundreds = '0'; hundreds <= '9'; hundred++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (tens = '0'; ten <= '9'; ten++)
 		{
-			if (ones != tens)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
-				putchar(tens);
-				putchar(ones);
-
-				if (!(ones == '9' && tens == '8'))
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
-
-	putchar('\n');
 
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a program with all possible combinations of two two-digit numbers
+ *
+ * Return: 0 (win)
+ */
+
+int main(void)
+{
+
