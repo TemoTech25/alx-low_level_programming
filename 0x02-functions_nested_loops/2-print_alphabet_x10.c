4eb6205911acd17f74858ffa933e6aefe9c98dca
Temo <temotech25@gmail.com>
2023-10-14 23:26:32 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: 0(Win)
+ */
+
+void print_alphabet_x10(void)
+{
+	char num;
+        char letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+	}
+}
+
