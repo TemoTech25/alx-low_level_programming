29fc545dd0ff6e325467c8475ae13c592fad0e93
Temo <temotech25@gmail.com>
2023-10-22 14:49:05 -0700

    fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,9 @@
 /**
  * reset_to_98 - print a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input   variable 
- * Return: n success 
+ *
+ * @n: input   variable
+ * Return: n success
  */
 
 void reset_to_98(int *n)
