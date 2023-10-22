55dd7430e7e9264163a08e86bbc6b97d3c8850bb
Temo <temotech25@gmail.com>
2023-10-22 14:38:49 -0700

    0-main.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - print a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: input   variable 
+ * Return: n success 
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - print swaps the values of two int
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
+
