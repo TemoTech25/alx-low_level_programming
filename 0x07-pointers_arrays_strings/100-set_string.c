31bfe76f72c7850a3fc9fcff696ddbe1522c6df1
Temo <temotech25@gmail.com>
2023-10-23 20:15:43 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - print sets the value 
+ * @s: rite pointaaother pointer 
+ * @to: pointer char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
