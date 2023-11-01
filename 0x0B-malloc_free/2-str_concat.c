2901105c46429cfc25978a12a7f6e501dd1c11d1
TemoTech25 <temotech25@gmail.com>
2023-10-31 19:03:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,30 +10,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	unsigned int len1, len2, i;
+   char *s;
+   unsigned int i, j, k;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	s = malloc((len1 + len2 + 1) * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	while (i < len1)
-	{
-	s[i] = s1[i];
-	i++;
-	}
-	i = 0;
-	while (i < len2)
-	{
-		s[len1 + i] = s2[i];
-		i++;
-	}
-	s[len1 + len2] = '\0';
-	return (s);
+   if (s1 == NULL)
+       s1 = "";
+   if (s2 == NULL)
+       s2 = "";
+   for (i = 0; s1[i] != '\0'; i++)
+       ;
+   for (j = 0; s2[j] != '\0'; j++)
+       ;
+   s = malloc((i + j + 1) * sizeof(char));
+   if (s == NULL)
+       return (NULL);
+   for (k = 0; k < i; k++)
+       s[k] = s1[k];
+   for (k = 0; k < j; k++)
+       s[i + k] = s2[k];
+   s[i + j] = '\0';
+   return (s);
 }
