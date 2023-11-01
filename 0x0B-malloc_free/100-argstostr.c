dbd1fbdadb2a5e960abdf4a15f085cd89549b44d
TemoTech25 <temotech25@gmail.com>
2023-10-31 19:53:15 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,22 +12,27 @@
 char *argstostr(int ac, char **av)
 {
     char *s;
-    int i, j, k, str_length = 0;
+    int i, j, k, total_length = 0;
 
     if (ac == 0 || av == NULL)
         return (NULL);
     for (i = 0; i < ac; i++)
-        str_length += strlen(av[i]);
-    s = malloc((str_length + ac) * sizeof(char));
+        total_length += strlen(av[i]);
+    total_length += ac;  // Add the count of arguments for the newline characters
+    s = malloc((total_length + 1) * sizeof(char));  // Add 1 for the null terminator
     if (s == NULL)
         return (NULL);
     k = 0;
     for (i = 0; i < ac; i++)
     {
         for (j = 0; av[i][j] != '\0'; j++)
-            s[k++] = av[i][j];
-        s[k++] = '\n';
+        {
+            s[k] = av[i][j];
+            k++;
+        }
+        s[k] = '\n';
+        k++;
     }
-    s[k] = '\0';
+    s[k] = '\0';  // Null terminate the string
     return (s);
 }
