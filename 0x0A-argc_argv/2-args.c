0953d7d25d06aaedbee81476a5cca13d03b49f55
TemoTech25 <temotech25@gmail.com>
2023-11-02 06:37:47 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#includie <stdio.h>
 #include "main.h"
 
 /**
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (int i = 0; i < argc; i++) 
+	for (i = 0; i < argc; i++) 
 	{
 		printf("%s\n", argv[i]);
 	}
