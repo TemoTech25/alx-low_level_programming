f527a20f13ba8d13e88a5b59d13d65fa5a6bf260
TemoTech25 <temotech25@gmail.com>
2023-11-02 08:59:52 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,35 +10,35 @@
 
 int _atoi(char *s)
 {
-  int i, n, len, f, digit;
+	int i, n, len, f, digit;
 
-  i = 0;
-  n = 0;
-  len = 0;
-  f = 0;
-  digit = 0;
+	i = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-  while (s[len] != '\0')
-      len++;
+	while (s[len] != '\0')
+		len++;
 
-  while (i < len && f == 0)
-  {
-      if (s[i] < '0' || s[i] > '9')
-          return (-1);
+	while (i < len && f == 0)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (-1);
 
-      digit = s[i] - '0';
-      n = n * 10 + digit;
-      f = 1;
-      if (s[i + 1] < '0' || s[i + 1] > '9')
-          break;
-      f = 0;
-      i++;
-  }
+		digit = s[i] - '0';
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+			break;
+		f = 0;
+		i++;
+	}
 
-  if (f == 0)
-      return (0);
+	if (f == 0)
+		return (0);
 
-  return (n);
+	return (n);
 }
 
 /**
@@ -49,25 +50,25 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-  int i, sum = 0;
+	int i, sum = 0;
 
-  if (argc == 1)
-  {
-      printf("0\n");
-      return (0);
-  }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-  for (i = 1; i < argc; i++)
-  {
-      if (_atoi(argv[i]) == -1)
-      {
-          printf("Error\n");
-          return (1);
-      }
-      sum += _atoi(argv[i]);
-  }
+	for (i = 1; i < argc; i++)
+	{
+		if (_atoi(argv[i]) == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += _atoi(argv[i]);
+	}
 
-  printf("%d\n", sum);
+	printf("%d\n", sum);
 
-  return (0);
+	return (0);
 }
