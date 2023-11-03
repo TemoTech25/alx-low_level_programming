440287d5e7b4ed3cc393f3b050dbf02f7dc93c03
TemoTech25 <temotech25@gmail.com>
2023-11-02 18:30:29 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *p;
+	void *p;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    p = malloc(nmemb * size);
-    if (!p)
-        return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
 
-    memset(p, 0, nmemb * size);
+	memset(p, 0, nmemb * size);
 
-    return (p);
+	return (p);
 }
-
