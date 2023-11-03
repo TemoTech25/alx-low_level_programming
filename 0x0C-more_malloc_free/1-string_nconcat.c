0e81a2b62d4186aac7ace4dbeceb1bc74b13063f
TemoTech25 <temotech25@gmail.com>
2023-11-02 18:09:30 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,19 +14,19 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = strlen(s1), len2 = strlen(s2);
-    char *new_string;
+	unsigned int len1 = strlen(s1), len2 = strlen(s2);
+	char *new_string;
 
-    if (n > len2)
-        n = len2;
+	if (n > len2)
+		n = len2;
 
-    new_string = malloc(len1 + n + 1);
-    if (!new_string)
-        return (NULL);
+	new_string = malloc(len1 + n + 1);
+	if (!new_string)
+		return (NULL);
 
-    strcpy(new_string, s1);
-    strncpy(new_string + len1, s2, n);
-    new_string[len1 + n] = '\0';
+	strcpy(new_string, s1);
+	strncpy(new_string + len1, s2, n);
+	new_string[len1 + n] = '\0';
 
-    return (new_string);
+	return (new_string);
 }
