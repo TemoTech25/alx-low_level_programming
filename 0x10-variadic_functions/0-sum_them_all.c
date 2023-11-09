1b2755d4c87854fe259d3bbd3fd4f9b42a696e91
TemoTech25 <temotech25@gmail.com>
2023-11-09 03:50:40 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  * Return: sum of all parameters
  *
  * Example:
- *   sum_them_all(4, 98, 1024, 402, -1024); --> 500
+ * sum_them_all(4, 98, 1024, 402, -1024); --> 500
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
- va_list ap;
- unsigned int i, sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
- va_start(ap, n);
+	va_start(ap, n);
 
- for (i = 0; i < n; i++)
-   sum += va_arg(ap, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
- va_end(ap);
+	va_end(ap);
 
- return (sum);
+	return (sum);
 }
