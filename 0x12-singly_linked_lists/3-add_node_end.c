5183d235adb5c18c44b64a5cb0ddeacf840c3802
TemoTech25 <temotech25@gmail.com>
2023-11-16 15:28:46 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,43 +11,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-   if (*head == NULL)
-   {
-       *head = malloc(sizeof(list_t));
-       if (*head == NULL)
-           return NULL;
+  list_t *new_node = malloc(sizeof(list_t));
+  if (new_node == NULL)
+      return NULL;
 
-       (*head)->str = strdup(str);
-       if ((*head)->str == NULL)
-       {
-           free(*head);
-           return NULL;
-       }
-       (*head)->len = strlen(str);
-       (*head)->next = NULL;
+  new_node->str = strdup(str);
+  if (new_node->str == NULL)
+  {
+      free(new_node);
+      return NULL;
+  }
+  new_node->len = strlen(str);
 
-       return *head;
-   }
+  if (*head == NULL)
+  {
+      *head = new_node;
+      new_node->next = NULL;
+      return new_node;
+  }
 
-   list_t *last_node = *head;
-   while (last_node->next != NULL)
-       last_node = last_node->next;
+  list_t *last_node = *head;
+  while (last_node->next != NULL)
+      last_node = last_node->next;
 
-   list_t *new_node = malloc(sizeof(list_t));
-   if (new_node == NULL)
-       return NULL;
+  last_node->next = new_node;
+  new_node->next = NULL;
 
-   new_node->str = strdup(str);
-   if (new_node->str == NULL)
-   {
-       free(new_node);
-       return NULL;
-   }
-   new_node->len = strlen(str);
-
-   new_node->next = NULL;
-   last_node->next = new_node;
-
-   return new_node;
+  return new_node;
 }
 
