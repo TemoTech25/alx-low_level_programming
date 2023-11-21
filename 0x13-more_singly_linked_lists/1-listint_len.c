93dbb1a46ce683a5e4eb7724b4031d700023faed
TemoTech25 <temotech25@gmail.com>
2023-11-21 15:59:01 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a listint_t list.
+ * listint_len - Returns the number of elements listint_t list
  * @h: Pointer to the head of the list.
  *
  * Return: The number of nodes.
@@ -18,5 +18,5 @@ size_t listint_len(const listint_t *h)
 		counter++;
 	}
 
-	return counter;
+	return (counter);
 }
