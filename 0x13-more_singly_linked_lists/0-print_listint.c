b339b91c398c0dc9b827d9bb62b6a67aaffc8f95
TemoTech25 <temotech25@gmail.com>
2023-11-21 16:35:01 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,10 +14,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		rintf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		counter++;
 	}
 
-	return counter;
+	return (counter);
 }
