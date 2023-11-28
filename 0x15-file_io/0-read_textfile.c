a7f73e64dc24c45f4da669b4bb86b9d074881cc5
TemoTech25 <temotech25@gmail.com>
2023-11-27 16:39:11 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,45 +11,31 @@
  * If filename is NULL return 0.
  * If write fails or does not write the expected amount of bytes, return 0.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  ssize_t bytes_read, bytes_written;
-  char buffer[1024];
-  int fd;
+	int filename_d;
+	ssize_t nrd, nwr;
+	char *buf;
 
-  /* Check if filename is NULL */
-  if (filename == NULL)
-  {
-      return 0;
-  }
+	if (!filename)
+		return (0);
 
-  /* Open the file */
-  fd = open(filename, O_RDONLY);
-  if (fd == -1)
-  {
-      return 0;
-  }
+	filename_d = open(filename, O_RDONLY);
 
-  /* Read the file */
-  bytes_read = read(fd, buffer, letters);
-  if (bytes_read == -1)
-  {
-      close(fd);
-      return 0;
-  }
+	if (fd == -1)
+		return (0);
 
-  /* Write the data to the standard output */
-  bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-  if (bytes_written == -1)
-  {
-      close(fd);
-      return 0;
-  }
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-  /* Close the file */
-  close(fd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-  /* Return the number of bytes read */
-  return bytes_read;
+	close(filename_d);
+
+	free(buf);
+
+	return (nwr);
 }
-
