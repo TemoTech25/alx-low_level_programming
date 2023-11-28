6ba9bd47ff65a4b6731b120a972052135c960327
TemoTech25 <temotech25@gmail.com>
2023-11-27 16:17:38 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,55 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: The path to the file to read
- * @letters: The number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @filename: the name of the file to read.
+ * @letters: the number of letters to read and print.
  *
- * Return: The actual number of letters read and printed, or 0 if an error occurred
+ * Return: the actual number of letters it could read and print.
+ * If the file can not be opened or read, return 0.
+ * If filename is NULL return 0.
+ * If write fails or does not write the expected amount of bytes, return 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-   int fd;
-   ssize_t bytes_read, bytes_written;
-   char buffer[letters];
+  ssize_t bytes_read, bytes_written;
+  char buffer[1024];
+  int fd;
 
-   if (filename == NULL)
-       return (0);
+  /* Check if filename is NULL */
+  if (filename == NULL)
+  {
+      return 0;
+  }
 
-   fd = open(filename, O_RDONLY);
-   if (fd == -1)
-       return (0);
+  /* Open the file */
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+  {
+      return 0;
+  }
 
-   bytes_read = read(fd, buffer, letters);
-   if (bytes_read == -1)
-   {
-       close(fd);
-       return (0);
-   }
+  /* Read the file */
+  bytes_read = read(fd, buffer, letters);
+  if (bytes_read == -1)
+  {
+      close(fd);
+      return 0;
+  }
 
-   bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-   if (bytes_written != bytes_read)
-   {
-       close(fd);
-       return (0);
-   }
+  /* Write the data to the standard output */
+  bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+  if (bytes_written == -1)
+  {
+      close(fd);
+      return 0;
+  }
 
-   if (close(fd) == -1)
-       return (0);
+  /* Close the file */
+  close(fd);
 
-   return (bytes_read);
+  /* Return the number of bytes read */
+  return bytes_read;
 }
 
