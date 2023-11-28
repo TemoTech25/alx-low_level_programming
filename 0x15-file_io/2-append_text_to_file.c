ff35bb7a4c33d36d12560318c338b07ec5fd36ca
TemoTech25 <temotech25@gmail.com>
2023-11-27 17:20:47 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,41 +10,39 @@
  * Return: 1 on success and -1 on failure.
  * If the file can not be opened or written to, return -1.
  * If filename is NULL return -1.
- * If text_content is NULL, do not add anything to the file. Return 1 if the file exists and -1 if the file does not exist or if you do not have the required permissions to write the file.
+ * If text_content is NULL, do not add anything to the file.
+ * Return 1 if the file exists and -1 if the file does not exist
+ * or if you do not have the required permissions to write the file.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int fd;
-  ssize_t bytes_written;
+	int fd;
+	ssize_t bytes_written;
 
-  /* Check if filename is NULL */
-  if (filename == NULL)
-  {
-      return -1;
-  }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-  /* Open the file with append permissions */
-  fd = open(filename, O_WRONLY | O_APPEND);
-  if (fd == -1)
-  {
-      return -1;
-  }
 
-  /* Write the text content to the file */
-  if (text_content != NULL)
-  {
-      bytes_written = write(fd, text_content, strlen(text_content));
-      if (bytes_written == -1)
-      {
-          close(fd);
-          return -1;
-      }
-  }
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
 
-  /* Close the file */
-  close(fd);
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-  /* Return success */
-  return 1;
+	close(fd);
+
+	return (1);
 }
-
