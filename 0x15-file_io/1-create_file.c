2b78e1276c058d8c86e511a6cdabb9d0f763efa1
TemoTech25 <temotech25@gmail.com>
2023-11-27 16:59:21 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,39 +12,36 @@
  * If filename is NULL return -1.
  * If text_content is NULL create an empty file.
  */
+
 int create_file(const char *filename, char *text_content)
 {
-   int fd;
-   ssize_t bytes_written;
+	int fd;
+	ssize_t bytes_written;
 
-   /* Check if filename is NULL */
-   if (filename == NULL)
-   {
-       return -1;
-   }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-   /* Open the file with write permissions, creating it if it doesn't exist */
-   fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-   if (fd == -1)
-   {
-       return -1;
-   }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
 
-   /* Write the text content to the file */
-   if (text_content != NULL)
-   {
-       bytes_written = write(fd, text_content, strlen(text_content));
-       if (bytes_written == -1)
-       {
-           close(fd);
-           return -1;
-       }
-   }
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-   /* Close the file */
-   close(fd);
 
-   /* Return success */
-   return 1;
+	close(fd);
+
+	return (1);
 }
 
